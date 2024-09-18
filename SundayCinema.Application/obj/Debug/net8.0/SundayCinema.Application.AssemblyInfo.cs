9b79f3c2ecc34f16c57056a172641e6a9ed0aa2d
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2024-09-18 17:28:54 +0600

    Added gitignore file

--- SundayCinema.Application/obj/Debug/net8.0/SundayCinema.Application.AssemblyInfo.cs
+++ SundayCinema.Application/obj/Debug/net8.0/SundayCinema.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SundayCinema.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc749f73fa56a25b589e73cccc2da01ab3bf71b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d1ad18910a73425b633e50525cc5062ba79a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("SundayCinema.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SundayCinema.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
